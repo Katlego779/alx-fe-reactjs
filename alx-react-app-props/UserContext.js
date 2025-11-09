4157c9cab75aa90ca7e58a2a0eafce68aed14ef5
Katlego Coto <katlegocoto305@gmail.com>
2025-11-09 20:18:39 +0200

    Move files into correct project and fix context API

--- alx-react-app-props/UserContext.js
+++ alx-react-app-props/UserContext.js
@@ -1,11 +1,9 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 
-// Create the context
 export const UserContext = createContext();
 
-// Create the provider
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ name: "", age: 0 }); // default empty state
+  const [user, setUser] = useState({ name: "", age: 0 });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
