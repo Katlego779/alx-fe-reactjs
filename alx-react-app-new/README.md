25b41d1c830fdce9af39f875cda8ea456a2b2e55
Katlego Coto <katlegocoto305@gmail.com>
2025-11-09 16:36:58 +0200

    Add alx-react-app-new files (Counter and app files)

---
+++ alx-react-app-new/README.md
@@ -0,0 +1,5 @@
+# alx-fe-reactjs
+
+This repository contains my first React app created using Vite for the ALX frontend curriculum.
+
+React + Vite
---
+++ alx-react-app-new/src/main.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
