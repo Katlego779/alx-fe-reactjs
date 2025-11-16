deee2f38c2dd7c2fe4060e293a29bb3c325fe07b
Katlego <katlegocoto305@gmail.com>
2025-11-16 17:06:49 +0200

    Update RecipeList to use useRecipeStore hook
    Refactor RecipeList component to use recipeStore.

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,3 +1,22 @@
+function RecipeList(){
+  import { useRecipeStore } from './recipeStore';
+
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
+
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+}
 import React from 'react';
 import { useRecipeStore } from '../stores/recipeStore';
 
