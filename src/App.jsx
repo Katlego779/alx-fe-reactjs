78a7f08d8dc422a68ab6e9c4c85fff52ebf7f7e6
Katlego <katlegocoto305@gmail.com>
2025-11-16 17:09:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
+import AddRecipeForm from './src/components/AddRecipeForm';
+import RecipeList from './src/components/RecipeList';
 
 function App() {
   return (
