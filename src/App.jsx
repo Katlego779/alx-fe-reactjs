f7a1afdb5b753e3b89f4897ae173e16861efa963
Katlego <katlegocoto305@gmail.com>
2025-11-16 17:10:36 +0200

    Fix import paths for components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import AddRecipeForm from './src/components/AddRecipeForm';
-import RecipeList from './src/components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
