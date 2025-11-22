289cd34880c01fc0f7458275628430f16e670248
Katlego Coto <katlegocoto305@gmail.com>
2025-11-22 10:33:47 +0200

    Fix: Implement Context API and remove prop drilling

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserContext from './UserContext';
+import UserProfile from './components/UserProfile';
+
+function App() {
+const userData = { name: 'Jane Doe', email: '[jane.doe@example.com](mailto:jane.doe@example.com)' };
+
+return (
+<UserContext.Provider value={userData}> <UserProfile />
+</UserContext.Provider>
+);
+}
+
+export default App;
