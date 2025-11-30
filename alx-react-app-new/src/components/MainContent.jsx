9eee27c95042929b4d6b2587f8119b9f671d4931
Katlego Coto <katlegocoto305@gmail.com>
2025-11-30 18:26:53 +0200

    Restore Tailwind setup and local changes

--- alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-    return (
-        <main style={{ backgroundColor: '#f0f8ff', padding: '15px', margin: '10px', borderRadius: '8px' }}>
-            <p style={{ fontSize: '16px', color: '#333' }}>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}
-
-export default MainContent;
